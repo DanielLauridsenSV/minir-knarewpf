4dbc4125c51e1101fe5f5ca812f8593aaff1a490
Daniel Lauridsen <daniel@pingas.se>
2021-12-17 12:58:14 +0100

    Made a working calculator

---
+++ miniräknarewpf/Classlibrary/Calculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniräknarewpf.Classlibrary
+{
+    internal class Calculator
+    {
+        public string Calculate(string results)
+        {
+            List<string> expression = interpretexpression(results);
+            return DoExpression(expression);
+
+        }
+        public List<string> interpretexpression(string results)
+        {
+ 
+            string operations = "";
+            string firstsum = "";
+            string secondsum = "";
+            for (int i = 0; i < results.Length; i++)
+            {
+                if (results[i] == '*' || results[i] == '/' || results[i] == '+' || results[i] == '-')
+                {
+                    operations = results[i].ToString();
+                    continue;
+                }
+               else if (string.IsNullOrEmpty(operations))
+                {
+                    firstsum += results[i].ToString();
+                }
+                else
+                {
+                    secondsum += results[i].ToString();
+                }
+            }
+            return new List<string> { firstsum, secondsum, operations };
+        }
+        public string DoExpression(List<string> expression)
+        {
+            string firstsum = expression[0];
+            string secondsum = expression[1];
+            string operaton = expression[2];
+
+            switch (operaton)
+            {
+                case "*":
+                    {
+                        return (int.Parse(firstsum) * int.Parse(secondsum)).ToString();
+                    }
+                case "/":
+                    {
+                        return (int.Parse(firstsum) / int.Parse(secondsum)).ToString();
+                    }
+                case "+":
+                    {
+                        return (int.Parse(firstsum) + int.Parse(secondsum)).ToString();
+                    }
+                case "-":
+                    {
+                        return (int.Parse(firstsum) - int.Parse(secondsum)).ToString();
+                    }
+                default:
+                    return "0";
+               
+            }
+        }
+    }
+}
--- miniräknarewpf/MainWindow.xaml.cs
+++ miniräknarewpf/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using miniräknarewpf.Classlibrary;
 
 namespace miniräknarewpf
 {
@@ -24,5 +25,101 @@ namespace miniräknarewpf
         {
             InitializeComponent();
         }
+
+        private void Checkresultsview()
+        {
+            if (Results.Text =="resultat")
+            {
+                Results.Text = "";
+            }
+        }
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "1";
+        }
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "2";
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "3";
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "4";
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "5";
+        }
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "6";
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "7";
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "8";
+        }
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "9"; 
+        }
+
+        private void Muliply_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "*";
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text = "";
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            Calculator calc = new();
+            Results.Text = calc.Calculate(Results.Text);
+        }
+
+        private void Subtraktion_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "-";
+        }
+
+        private void Divided_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "/";
+        }
+
+        private void Addition_Click(object sender, RoutedEventArgs e)
+        {
+            Checkresultsview();
+            Results.Text += "+";
+        }
+
+
     }
 }
